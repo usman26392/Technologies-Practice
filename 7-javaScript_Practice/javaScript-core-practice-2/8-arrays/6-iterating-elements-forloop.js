2d3f0c7a5b38c3cef6542f321fa895fbacc214d5
Muhammad Usman <usmannawaz53@gmail.com>
2021-11-11 23:56:46 +0500

    folder sequences

---
+++ 7-javaScript_Practice/javaScript-core-practice-2/8-arrays/6-iterating-elements-forloop.js
@@ -0,0 +1,52 @@
+/**
+ * iterating elements of Array.
+ */
+
+
+/**
+ * --------------- by for loop ---------------------
+ */
+
+//  let colors = ["red", "green", "blue"];
+//  for(let i = 0; i < colors.length; i++) {
+//      console.log(colors[i]);
+//  }
+
+
+/**
+ * example-2
+ */
+
+//  let random_No = [5,10,20,10,30];
+//  let sum = 0;
+//  for(let i = 0; i < random_No.length; i++ ) {
+//      sum = sum + random_No[i];
+//     //  OR
+//     //  sum += random_No[i];
+//  }
+//  console.log(` This is sum of random Numbers of Array:  ${sum}`);
+
+
+// example - 2
+var arr = [1,2,3,4,5];
+
+// for(let i = 0; i < arr.length; i++) {
+//     console.log(arr[i] * arr[i]);
+// }
+
+// OR
+
+arr.forEach(function(elm, index, theArray) {
+    console.log(elm * elm);
+    // console.log(theArray[index]);
+    // console.log(theArray[index] = elm * elm);
+});
+
+// now change "arr" with "theArray"
+console.log(arr);
+
+
+
+
+
+
